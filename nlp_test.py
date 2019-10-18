e5a6e4a3002673d6aa80b26692e390045a37577e
Vishal Vijayakumar <vvishal@cisco.com>
2019-10-18 00:53:26 -0700

    init commit

---
+++ nlp_test.py
@@ -0,0 +1,14 @@
+from mindmeld import NaturalLanguageProcessor
+nlp = NaturalLanguageProcessor(app_path='blitz')
+nlp.build()
+a = nlp.process(query_text='hi there')
+print(a)
+
+
+
+# Look at PFF
+# Look at  DVOA ranking
+# News from around the legauge
+# What the weather today in X
+# Show play from last week's game
+# Who are the refs what are their tendancies
