b6c762415be8ab1f4fed3761c13414c462a2b55b
Alex Huang <alhuang10@gmail.com>
2019-10-30 16:18:19 -0700

    DVOA intent handling

--- blitz/nfl_intents.py
+++ blitz/nfl_intents.py
@@ -4,9 +4,48 @@ from .nfl.team import Team
 import requests
 import lxml.html as lh
 from collections import defaultdict
+import json
+from blitz.root import NFL_DATA_PATH
+import pandas as pd
+import re
 
 
 logger = get_logger()
+
+CANONICAL_NAME_TO_ABBRVIATION = {
+    "49ers": "SF",
+    "Bears": "CHI",
+    "Bengals": "CIN",
+    "Bills": "BUF",
+    "Broncos": "DEN",
+    "Browns": "CLE",
+    "Buccaneers": "TB",
+    "Cardinals": "ARI",
+    "Chargers": "LAC",
+    "Chiefs": "KC",
+    "Colts": "IND",
+    "Cowboys": "DAL",
+    "Dolphins": "MIA",
+    "Eagles": "PHI",
+    "Falcons": "ATL",
+    "Giants": "NYG",
+    "Jaguars": "JAX",
+    "Jets": "NYJ",
+    "Lions": "DET",
+    "Packers": "GB",
+    "Panthers": "CAR",
+    "Patriots": "NE",
+    "Raiders": "OAK",
+    "Rams": "LAR",
+    "Ravens": "BAL",
+    "Redskins": "WAS",
+    "Saints": "NO",
+    "Seahawks": "SEA",
+    "Steelers": "PIT",
+    "Texans": "HOU",
+    "Titans": "TEN",
+    "Vikings": "MIN",
+}
 
 
 @app.handle(domain='nfl', intent='get_depth_chart')
@@ -62,7 +101,56 @@ def specify_position(request, responder):
 
 @app.handle(domain='nfl', intent='get_dvoa')
 def get_dvoa(request, responder):
-    responder.reply("Here is the dvoa")
+    dvoa_json_file = f'{NFL_DATA_PATH}/league.json'
+    with open(dvoa_json_file) as json_file:
+        league_dvoa_data = json.load(json_file)
+
+    dvoa_type_entity = [x for x in request.entities if x['type'] == 'dvoa_type']
+    dvoa_type = dvoa_type_entity[0]['value'][0]['cname']
+
+    if dvoa_type == "Offensive":
+        dvoa_link = league_dvoa_data['DVOA']['offense']
+        dvoa_column = "OFFENSEDVOA"
+    elif dvoa_type == "Defensive":
+        dvoa_link = league_dvoa_data['DVOA']['defense']
+        dvoa_column = "DEFENSEDVOA"
+    elif dvoa_type == "Overall":
+        dvoa_link = league_dvoa_data['DVOA']['overall']
+        dvoa_column = "TOTALDVOA"
+    else:
+        dvoa_link = league_dvoa_data['DVOA']['special']
+        dvoa_column = "S.T.DVOA"
+
+    page = requests.get(dvoa_link)
+    doc = lh.fromstring(page.content)
+    tr_elements = doc.xpath('//tr')
+
+    columns = []
+    for t in tr_elements[0]:
+        name = re.sub(r"[\n\t\s]*", "", t.text_content().strip())
+        columns.append((name, []))
+
+    for t in tr_elements[1:]:
+        children = t.getchildren()
+        for i, c in enumerate(children):
+            data = c.text_content().lstrip().rstrip()
+            try:
+                data = float(data)
+            except:
+                pass
+
+            columns[i][1].append(data)
+
+    dvoa_dict = {title:column for (title,column) in columns}
+    dvoa_df = pd.DataFrame(dvoa_dict)
+
+    team_entities = [x for x in request.entities if x['type'] == 'team']
+    canonical_team_name = team_entities[0]['value'][0]['cname']
+
+    team_abbreviation = CANONICAL_NAME_TO_ABBRVIATION[canonical_team_name]
+
+    dvoa_value = dvoa_df[dvoa_df['TEAM'] == team_abbreviation][dvoa_column].iloc[0]
+    responder.reply(f"The {canonical_team_name} {dvoa_type.lower()} DVOA is {dvoa_value}")
 
 
 @app.handle(domain='nfl', intent='get_injury_report')
